2c4d45ab6f355609e4c146f4ff98396821f5efe0
F.M. Mendoza <mendoza.fernando@hotmail.com>
2025-05-14 21:24:27 -0500

    upxdate

---
+++ insert-header.js
@@ -0,0 +1,9 @@
+document.addEventListener("DOMContentLoaded", () => {
+  fetch("header.html")
+    .then(response => response.text())
+    .then(data => {
+      document.getElementById("header").innerHTML = data;
+    })
+    .catch(error => console.error("Error al cargar el header:", error));
+});
+
